9a37a19355c8611256b36be8eb4ae90f1f98ab84
Pieter Bommele <41929540+piebom@users.noreply.github.com>
2022-10-16 03:34:42 +0200

    update

--- lefishe/src/App.js
+++ lefishe/src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import CustomHeader from './components/CustomHeader';
-import ReactAudioPlayer from 'react-audio-player';
-import { useEffect } from 'react';
 import lefishe from './assets/lefishe.mp3';
-import Player from './components/Player';
 function App() {
   // set window title
   document.title = "Le Fishe - Home";
-  useEffect(() => {
-    var sample = document.getElementById("foobar");
-    sample.play();  
-  }, []);
   return (
     <div className="App">
        <audio id="foobar" src={lefishe} preload="auto"/> 
